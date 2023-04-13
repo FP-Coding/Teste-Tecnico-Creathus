0fa022a05a7e4e90d347d93b9d22762717bdb7ef
Felipe Pinto <felipe14062000@gmail.com>
2023-04-13 16:03:47 -0300

    feat: criação das interfaces IMovie, IMovieCardProps e ICreateMovieInfo

---
+++ frontend/src/interfaces/IMovieCardProps.ts
@@ -0,0 +1,4 @@
+export default interface IMovieCardProps {
+  image: string,
+  title: string
+}
---
+++ frontend/src/interfaces/IMovieInfo.ts
@@ -0,0 +1,10 @@
+export default interface ICreateMovieInfo {
+  author: string,
+  title: string,
+  description: string,
+  image: string,
+}
+
+export interface IMovieInfo extends ICreateMovieInfo {
+  id?: string,
+}
