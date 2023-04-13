f76fe96eef8b30cc0bde6a398c130b067ff3937b
Felipe Pinto <felipe14062000@gmail.com>
2023-04-12 21:11:09 -0300

    feat: Criação do router Movie

---
+++ backend/src/Api/Routes/Movie.router.ts
@@ -0,0 +1,14 @@
+import { Request, Response, Router } from 'express';
+import MovieODM from '../../Database/Models/MovieODM';
+import MovieService from '../Services/Movie.service';
+import MovieController from '../Controllers/Movie.controller';
+
+const route = Router();
+const modelBase = new MovieODM();
+const serviceBase = new MovieService(modelBase);
+const controllerBase = new MovieController(serviceBase);
+
+route.get('/:id', (req: Request, res: Response) => controllerBase.getById(req, res));
+route.get('/', (req: Request, res: Response) => controllerBase.getAll(req, res));
+
+export default route;
