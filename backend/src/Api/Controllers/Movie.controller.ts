e1e190c420613c352e79e184146c6d74fc4db2ec
Felipe Pinto <felipe14062000@gmail.com>
2023-04-12 23:08:01 -0300

    feat: criação da função create do MovieController

--- backend/src/Api/Controllers/Movie.controller.ts
+++ backend/src/Api/Controllers/Movie.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import IMovieController from '../Interfaces/IMovieController';
 import IMovieService from '../Interfaces/IMovieService';
+import Movie from '../Domain/Movie';
+import IMovie from '../Interfaces/IMovie';
 
 class MovieController implements IMovieController {
   private _service: IMovieService;
@@ -17,6 +19,18 @@ class MovieController implements IMovieController {
   async getById(req: Request, res: Response): Promise<Response> {
     const { id } = req.params;
     const movie = await this._service.getById(id);
+    return res.status(200).json(movie);
+  }
+
+  async create(req: Request, res: Response): Promise<Response> {
+    const infoMovie = req.body as IMovie;
+    const formatedInfoMovie = new Movie(
+      infoMovie.author,
+      infoMovie.title,
+      infoMovie.description,
+      infoMovie.image,
+    ).getInfoMovie();
+    const movie = await this._service.create(formatedInfoMovie);
     return res.status(200).json(movie);
   }
 }
--- backend/src/Api/Interfaces/IMovieController.ts
+++ backend/src/Api/Interfaces/IMovieController.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express';
 interface IMovieController {
   getAll(req: Request, res: Response): Promise<Response>,
   getById(req: Request, res: Response): Promise<Response>,
+  create(req: Request, res: Response): Promise<Response>,
 }
 
 export default IMovieController;
