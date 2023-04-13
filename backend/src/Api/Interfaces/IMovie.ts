920d982de331053f2abb4aec9de652223e83928c
Felipe Pinto <felipe14062000@gmail.com>
2023-04-12 23:05:37 -0300

    feat: aplicação da segregação de interface

--- backend/src/Api/Interfaces/IMovie.ts
+++ backend/src/Api/Interfaces/IMovie.ts
@@ -1,9 +1,12 @@
-interface IMovie {
-  id?: string,
+export interface INewMovie {
   author: string,
   title: string,
   description: string,
   image: string
+}
+
+interface IMovie extends INewMovie {
+  id?: string,
 }
 
 export default IMovie;
