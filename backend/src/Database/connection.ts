98d52293271afb74d27f2f444826f183a8091694
Felipe Pinto <felipe14062000@gmail.com>
2023-04-14 22:44:17 -0300

    feat: mudança da variavel de ambiente

--- backend/src/Database/connection.ts
+++ backend/src/Database/connection.ts
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose';
 import 'dotenv/config';
 
+const { MONGO_URL } = process.env;
+
 const MONGO_DB_URL = 'mongodb://localhost:27017/MoviesCreathus';
 
 const connectToDatabase = (
-  mongoDatabaseURI = process.env.MONGO_URI || MONGO_DB_URL,
+  mongoDatabaseURI = MONGO_URL || MONGO_DB_URL,
 ) => mongoose.connect(mongoDatabaseURI);
 
 export default connectToDatabase;
