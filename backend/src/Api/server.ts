b56ae76d84a01691d163f62fa18588c251e76b6d
Felipe Pinto <felipe14062000@gmail.com>
2023-04-14 23:04:38 -0300

    fix: mudança do nome da variável

--- backend/src/Api/server.ts
+++ backend/src/Api/server.ts
@@ -3,7 +3,7 @@ import 'dotenv/config';
 import app from './app';
 import connectToDatabase from '../Database/connection';
 
-const PORT = process.env.API_PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 connectToDatabase().then(() => {
   app.listen(PORT, () => console.log(`running on port ${PORT}`));
