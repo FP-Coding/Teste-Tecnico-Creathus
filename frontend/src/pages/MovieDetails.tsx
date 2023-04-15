22ae8767eaa62cf7b8728a31e9ae4d07261ff2d1
Felipe Pinto <felipe14062000@gmail.com>
2023-04-14 21:06:47 -0300

    feat: criação da página de detalhes do filme

--- frontend/src/pages/MovieDetails.tsx
+++ frontend/src/pages/MovieDetails.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
-import Header from '../components/Header.tsx';
+import { getRequest } from '../utils/axios.ts';
+import { IMovieId, IMovieInfo } from '../interfaces/IMovieInfo.ts';
+import HeaderDetails from '../components/HeaderDetails.tsx';
+import '../css/MovieDetails.css';
 
 function MovieDetails(props: RouteComponentProps) {
+  const [movie, setMovie] = useState<IMovieInfo>({
+    author: '', description: '', image: '', title: '', id: '',
+  });
+  const { match: { params } } = props;
+  const { id } = params as IMovieId;
+  useEffect(() => {
+    const request = async () => {
+      const response = await getRequest(`/movies/${id}`);
+      return setMovie(response);
+    };
+    request();
+  }, []);
+
   return (
-    <Header {...props} />
+    <div>
+      <HeaderDetails />
+      <div className="container-movie-details">
+        <div className="container-main-info-movie">
+          <div className="container-image">
+            <img src={movie.image} alt="imagem do filme" className="image-details" />
+          </div>
+          <h1 className="title-movie-details">{movie.title}</h1>
+        </div>
+        <div className="info-movie">
+          <h3>SINOPSE & INFO</h3>
+          <p className="sinopse-movie">{movie.description}</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
