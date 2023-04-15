5f882e5e1c3a274faf4960f4d846c84b40d79d2f
Felipe Pinto <felipe14062000@gmail.com>
2023-04-15 14:42:55 -0300

    feat: mudança lógica da variáveis de ambiente

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import IMovieInfo from '../interfaces/IMovieInfo';
 
-// const HOST = import.meta.env.REACT_APP_BACKEND_HOST || 'localhost:3001';
-// const PROTOCOL = import.meta.env.REACT_APP_PROTOCOL || 'http';
+const HOST = import.meta.env.REACT_APP_BACKEND_HOST;
+const PROTOCOL = import.meta.env.REACT_APP_PROTOCOL;
+
 const apiRailway = 'https://tiresome-agreement-production.up.railway.app';
-// const baseURL = `${PROTOCOL}://${HOST}`;
-const api = axios.create({ baseURL: apiRailway });
+const baseURL = HOST && PROTOCOL ? `${PROTOCOL}://${HOST}` : apiRailway;
+const api = axios.create({ baseURL });
 
 export const postRequest = async (endpoint: string, body: IMovieInfo) => {
   const { data } = await api.post(endpoint, body);
